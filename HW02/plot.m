00ac09a519307919627a4bb985017dc8a256b911
TTHuang5799 <167280781+TTHuang5799@users.noreply.github.com>
2024-09-29 13:37:26 -0500

    Update plot.m

--- HW02/plot.m
+++ HW02/plot.m
@@ -1,14 +1,16 @@
-% plot_task.m
-
 powers = 10:30;
 times = [];
 
 fid = fopen('task1.output', 'r');
 
+if fid == -1
+    error('Error: Cannot open file task1.output. Check if the file exists.');
+end
+
 for i = 1:length(powers)
-    fgetl(fid);
+    fgetl(fid); 
     times(i) = str2double(fgetl(fid));
-    fgetl(fid);
+    fgetl(fid); 
     fgetl(fid);
     fgetl(fid);
 end
