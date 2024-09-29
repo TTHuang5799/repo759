4cb40129f0ed3ff4731dbfa6e3d1b5313bd16f69
TTHuang5799 <167280781+TTHuang5799@users.noreply.github.com>
2024-09-29 16:56:24 -0500

    Update convolution.cpp

--- HW02/convolution.cpp
+++ HW02/convolution.cpp
@@ -4,7 +4,7 @@
 
 float check_f(const float *f, std::size_t i, std::size_t j, std::size_t n) {
 	if (0 <= i && i < n && 0 <= j && j < n) {
-		return f[i*n+j];
+		return f[i * n + j];
 	}
 	if ((0 <= i && i < n) || (0 <= j && j < n)) {
 		return 1;
@@ -18,13 +18,14 @@ void convolve(const float *image, float *output, std::size_t n, const float *mas
 		output[i] = 0;
 	}
 #pragma omp parallel for collapse(2)
-	for(std::size_t x = 0; x < n; x++) {
-		for(std::size_t y = 0; y < n; y++) {
-			for(std::size_t i = 0; i < m; i++) {
-				for(std::size_t j = 0; j < 0; j++) {
-					output[x * n + y] += mask[i * m + j] * check_f(image, x + i - ((m-1)/2), y + j - ((m-1)/2), n);
-				}
-			}
-		}
+	for (std::size_t x = 0; x < n; x++) {
+	    for (std::size_t y = 0; y < n; y++) {
+	        for (std::size_t i = 0; i < m; i++) {
+	            for (std::size_t j = 0; j < m; j++) {
+	                output[x * n + y] += mask[i * m + j] * check_f(image, x + i - (m - 1) / 2, y + j - (m - 1) / 2, n);
+	            }
+	        }
+	        cout << "output[" << x << ", " << y << "] = " << output[x * n + y] << endl;
+	    }
 	}
 }
