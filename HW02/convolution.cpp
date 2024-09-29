f4e555d5ed6df8f8d5362c17820907779ba8f7a9
TTHuang5799 <167280781+TTHuang5799@users.noreply.github.com>
2024-09-29 16:27:04 -0500

    Add files via upload

---
+++ HW02/convolution.cpp
@@ -0,0 +1,26 @@
+#include "convolution.h"
+
+float 2dto1d(const float *f, std::size_t i, std::size_t j, std::size_t n) {
+	if (0 <= i && i < n && 0 <= j && j < n)
+		return f[i*n+j];
+	if ((0 <= i && i < n) || (0 <= j && j < n))
+		return 1;
+	return 0;
+}
+
+void convolve(const float *image, float *output, std::size_t n, const float *mask, std::size_t m) {
+	//Initialize all to 0
+	for(size_t i = 0; i < n * n; i++) {
+		output[i] = 0;
+	}
+#pragma omp parallel for collapse(2)
+	for(std::size_t x = 0; x < n; x++) {
+		for(std:size_t y = 0; y < n; y++) {
+			for(std::size_t i = 0; i < m; i++) {
+				for(std:size_t j = 0; j < 0; j++) {
+					output[x * n + y] += mask[i * m + j] * 2dto1d(image, x + i - ((m-1)/2), y + j - ((m-1)/2), n)
+				}
+			}
+		}
+	}
+}
---
+++ HW02/task2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <cstdio>
+#include <cstdlib>
+#include "convolution.h"
+
+using namespace std;
+using namespace chrono;
+
+int main(int argc, char *argv[]) {
+
+	if (argc < 3) {
+		cout << "Usage: " << argv[0] << " <n> <m>" << endl;
+		return 1;
+    	}
+
+    	size_t n = atoi(argv[1]); 
+    	size_t m = atoi(argv[2]); 
+
+	random_device rd;
+	mt19937_64 generator(rd());
+
+	uniform_real_distribution<float> distImge(-10.0, 10.0);
+	uniform_real_distribution<float> distMask(-1.0, 1.0); 
+
+	float *image = new float[n*n];
+	float *mask = new float[m*m];
+	float *output = new float[n*n];
+
+    	for (size_t i = 0; i < n; ++i) {
+        	for (size_t j = 0; j < n; ++j) {
+            		image[i * n + j] = distImge(generator);
+        	}
+    	}
+
+    	for (size_t i = 0; i < m; ++i) {
+        	for (size_t j = 0; j < m; ++j) {
+            		mask[i * m + j] = distMask(generator);
+        	}
+    	}
+
+    	auto start = high_resolution_clock::now();
+    	convolve(image, output, n, mask, m); 
+    	auto stop = high_resolution_clock::now();
+
+	auto duration_ms = duration_cast<milliseconds>(stop - start);
+
+	cout << duration_ms.count() << endl;
+	cout << output[0] << endl;
+	cout << output[n-1] << endl;
+
+	delete[] image;
+	delete[] mask;
+	delete[] output;
+
+	return 0;
+}
