d088c582ead5576833fba74cd499fec53755cdf6
TTHuang5799 <167280781+TTHuang5799@users.noreply.github.com>
2024-09-29 21:19:34 -0500

    Update matmul.h

--- HW02/matmul.h
+++ HW02/matmul.h
@@ -4,9 +4,11 @@
 #include <cstddef>
 #include <vector>
 
+using namespace std;
+
 void mmul1(const double *A, const double *B, double *C, const unsigned int n);
 void mmul2(const double *A, const double *B, double *C, const unsigned int n);
 void mmul3(const double *A, const double *B, double *C, const unsigned int n);
-void mmul4(const std::vector<double> &A, const std::vector<double> &B, double *C, const unsigned int n);
+void mmul4(const vector<double> &A, const vector<double> &B, double *C, const unsigned int n);
 
 #endif
