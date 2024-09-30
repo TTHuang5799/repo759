7f2e5dfff9fb3dbba7094ea96e714799d1d86982
TTHuang5799 <167280781+TTHuang5799@users.noreply.github.com>
2024-09-29 21:12:49 -0500

    Create matmul.h

---
+++ HW02/matmul.h
@@ -0,0 +1,12 @@
+#ifndef MATMUL_H
+#define MATMUL_H
+
+#include <cstddef>
+#include <vector>
+
+void mmul1(const double* A, const double* B, double* C, const unsigned int n);
+void mmul2(const double* A, const double* B, double* C, const unsigned int n);
+void mmul3(const double* A, const double* B, double* C, const unsigned int n);
+void mmul4(const std::vector<double>& A, const std::vector<double>& B, double* C, const unsigned int n);
+
+#endif
