a951781e5b60cc8d9abe443d677b3f22cad01d30
TTHuang5799 <167280781+TTHuang5799@users.noreply.github.com>
2024-10-17 12:06:33 -0500

    Update matmul.cpp

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -1,1 +1,17 @@
+#include "matmul.h"
+#include <omp.h> // Include OpenMP header
+#include <iostream>
 
+void mmul(const double *A, const double *B, double *C, const unsigned int n) {
+    unsigned int i, j, k;
+    
+    // Parallelizing the outer loop with OpenMP
+    #pragma omp parallel for private(i, j, k) shared(A, B, C)
+    for (i = 0; i < n; i++) {
+        for (k = 0; k < n; k++) {
+            for (j = 0; j < n; j++) {
+                C[i * n + j] += A[i * n + k] * B[k * n + j];
+            }
+        }
+    }
+}
