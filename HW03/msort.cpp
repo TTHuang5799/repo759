b733a4b24be587a63ff2efb6b3ce428d710bf43b
TTHuang5799 <167280781+TTHuang5799@users.noreply.github.com>
2024-10-17 14:12:25 -0500

    Update msort.cpp

--- HW03/msort.cpp
+++ HW03/msort.cpp
@@ -2,23 +2,21 @@
 #include <omp.h>
 #include <iostream>
 
-using namespace std;
-
 // Function to merge two halves of the array
-void merge(int* arr, size_t left, size_t mid, size_t right) {
-    size_t n1 = mid - left + 1;
-    size_t n2 = right - mid;
+void merge(int* arr, std::size_t left, std::size_t mid, std::size_t right) {
+    std::size_t n1 = mid - left + 1;
+    std::size_t n2 = right - mid;
 
     int* leftArr = new int[n1];
     int* rightArr = new int[n2];
 
     // Copy data to temporary arrays
-    for (size_t i = 0; i < n1; i++)
+    for (std::size_t i = 0; i < n1; i++)
         leftArr[i] = arr[left + i];
-    for (size_t i = 0; i < n2; i++)
+    for (std::size_t i = 0; i < n2; i++)
         rightArr[i] = arr[mid + 1 + i];
 
-    size_t i = 0, j = 0, k = left;
+    std::size_t i = 0, j = 0, k = left;
 
     // Merge the two halves
     while (i < n1 && j < n2) {
@@ -49,9 +47,9 @@ void merge(int* arr, size_t left, size_t mid, size_t right) {
 }
 
 // Recursive merge sort function
-void msort(int* arr, size_t left, size_t right) {
+void msort(int* arr, std::size_t left, std::size_t right) {
     if (left < right) {
-        size_t mid = left + (right - left) / 2;
+        std::size_t mid = left + (right - left) / 2;
 
         // Parallelize the sorting of the two halves using OpenMP
         #pragma omp parallel sections
