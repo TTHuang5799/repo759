620a272988dc9fcff55cc042e04abe188ac1b749
TTHuang5799 <167280781+TTHuang5799@users.noreply.github.com>
2024-10-17 18:43:42 -0500

    Update msort.cpp

--- HW03/msort.cpp
+++ HW03/msort.cpp
@@ -1,77 +1,61 @@
-#include "msort.h"
 #include <omp.h>
+#include "msort.h"
 
-void merge(int* arr, std::size_t left, std::size_t mid, std::size_t right) {
-    std::size_t n1 = mid - left + 1;
-    std::size_t n2 = right - mid;
+// Merge function to merge two sorted halves
+void merge(int* arr, int* temp, int left, int mid, int right) {
+    int i = left;
+    int j = mid + 1;
+    int k = left;
 
-    // Allocate memory for temporary arrays
-    int* leftArr = new int[n1];
-    int* rightArr = new int[n2];
-
-    // Copy data to temp arrays
-    for (std::size_t i = 0; i < n1; i++) {
-        leftArr[i] = arr[left + i];
-    }
-    for (std::size_t i = 0; i < n2; i++) {
-        rightArr[i] = arr[mid + 1 + i];
-    }
-
-    std::size_t i = 0, j = 0, k = left;
-
-    // Merge the temp arrays back into arr
-    while (i < n1 && j < n2) {
-        if (leftArr[i] <= rightArr[j]) {
-            arr[k] = leftArr[i];
-            i++;
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
         } else {
-            arr[k] = rightArr[j];
-            j++;
+            temp[k++] = arr[j++];
         }
-        k++;
     }
 
-    // Copy remaining elements of leftArr[], if any
-    while (i < n1) {
-        arr[k] = leftArr[i];
-        i++;
-        k++;
+    while (i <= mid) {
+        temp[k++] = arr[i++];
     }
 
-    // Copy remaining elements of rightArr[], if any
-    while (j < n2) {
-        arr[k] = rightArr[j];
-        j++;
-        k++;
+    while (j <= right) {
+        temp[k++] = arr[j++];
     }
 
-    // Free the allocated memory for the temp arrays
-    delete[] leftArr;
-    delete[] rightArr;
+    for (int i = left; i <= right; i++) {
+        arr[i] = temp[i];
+    }
 }
 
-void msort(int* arr, std::size_t left, std::size_t right, std::size_t ts) {
-    if (right - left + 1 <= ts) {
-        // Serial insertion sort
-        for (std::size_t i = left + 1; i <= right; i++) {
-            int key = arr[i];
-            std::size_t j = i - 1;
-            while (j >= left && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-        }
-    } else {
-        std::size_t mid = left + (right - left) / 2;
-        
-        #pragma omp task shared(arr) if(right - left > ts)
-        msort(arr, left, mid, ts);
-        
-        #pragma omp task shared(arr) if(right - left > ts)
-        msort(arr, mid + 1, right, ts);
-        
-        #pragma omp taskwait
-        merge(arr, left, mid, right);
+// Recursive parallel merge sort
+void parallel_merge_sort(int* arr, int* temp, int left, int right, int ts) {
+    if (right - left <= ts) {
+        // Base case: small enough array, use serial sort
+        std::sort(arr + left, arr + right + 1);
+        return;
     }
+
+    int mid = left + (right - left) / 2;
+
+    // Parallelize the recursive calls with tasks
+    #pragma omp task shared(arr, temp)
+    parallel_merge_sort(arr, temp, left, mid, ts);
+
+    #pragma omp task shared(arr, temp)
+    parallel_merge_sort(arr, temp, mid + 1, right, ts);
+
+    #pragma omp taskwait
+    merge(arr, temp, left, mid, right);
+}
+
+// Interface function called by task3.cpp
+void msort(int* arr, int n, int ts) {
+    int* temp = new int[n];
+    #pragma omp parallel
+    {
+        #pragma omp single
+        parallel_merge_sort(arr, temp, 0, n - 1, ts);
+    }
+    delete[] temp;
 }
